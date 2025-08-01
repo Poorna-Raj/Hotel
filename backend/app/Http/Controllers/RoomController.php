c5d5297defc56c7fb91f91fe309546348962dec5
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-01 17:02:03 +0530

    Implements the room CRUD

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Room;
-use App\Http\Requests\StoreRoomRequest;
-use App\Http\Requests\UpdateRoomRequest;
+use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
@@ -13,15 +13,53 @@ class RoomController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $rooms = Room::all();
+
+            return response()->json([
+                "success" => true,
+                "message" => "Rooms retrieved successfully",
+                "data" => $rooms
+            ], 200);
+
+        } catch (Exception $ex) {
+            return response()->json([
+                "success" => false,
+                "message" => "Something went wrong",
+                "error" => $ex->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRoomRequest $request)
+    public function store(Request $request)
     {
-        //
+
+        $feilds = $request->validate([
+            "name" => "required|string|max:100|unique:rooms",
+            "number" => "required|integer|unique:rooms",
+            "price" => "required|numeric",
+            "occupancy" => "required|integer|min:1",
+            "bed_type" => "required|string",
+            "room_type" => "required|string",
+            "hasac" => "required|boolean",
+            "status" => "required|string"
+        ]);
+        try {
+            Room::create($feilds);
+            return response()->json([
+                "success" => true,
+                "message" => "Room Created Successfully"
+            ], 201);
+        } catch (Exception $ex) {
+            return response()->json([
+                "success" => false,
+                "message" => "Failed to create room",
+                "error" => $ex->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -29,15 +67,49 @@ class RoomController extends Controller
      */
     public function show(Room $room)
     {
-        //
+        try {
+            return response()->json([
+                "success" => true,
+                "message" => "Room Retrived Successfully",
+                "data" => $room
+            ]);
+        } catch (Exception $ex) {
+            return response()->json([
+                "success" => false,
+                "message" => "Failed to retrive room",
+                "error" => $ex->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRoomRequest $request, Room $room)
+    public function update(Request $request, Room $room)
     {
-        //
+        $feilds = $request->validate([
+            "name" => "required|string|max:100|unique:rooms",
+            "number" => "required|integer|unique:rooms",
+            "price" => "required|numeric",
+            "occupancy" => "required|integer|min:1",
+            "bed_type" => "required|string",
+            "room_type" => "required|string",
+            "hasac" => "required|boolean",
+            "status" => "required|string"
+        ]);
+        try {
+            $room->update($feilds);
+            return response()->json([
+                "success" => true,
+                "message" => "Room Updated Successfully"
+            ], 200);
+        } catch (Exception $ex) {
+            return response()->json([
+                "success" => false,
+                "message" => "Failed to update room",
+                "error" => $ex->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -45,6 +117,18 @@ class RoomController extends Controller
      */
     public function destroy(Room $room)
     {
-        //
+        try {
+            $room->delete();
+            return response()->json([
+                "success" => true,
+                "message" => "Room Deleted Successfully"
+            ]);
+        } catch (Exception $ex) {
+            return response()->json([
+                "success" => false,
+                "message" => "Failed to delete room",
+                "error" => $ex->getMessage()
+            ], 500);
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/logout", [AuthController::class, "logout"])->middleware("auth:sanctum");
+
+Route::apiResource("rooms", RoomController::class);
